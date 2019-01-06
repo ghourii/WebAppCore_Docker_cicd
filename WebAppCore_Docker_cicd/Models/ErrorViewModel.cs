eefa906b93fe78179e84e9501e2aa2095daf8a9b
Asif ghouri <asifarifghouri@hotmail.com>
2019-01-06 23:16:55 +0500

    Add project files.

---
+++ WebAppCore_Docker_cicd/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppCore_Docker_cicd.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
